f8c576e9024bcf280422a4df48c696ae37d03a77
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2023-03-08 19:39:25 -0800

    checkbox, comments, and math

--- src/components/ROI.js
+++ src/components/ROI.js
@@ -7,11 +7,34 @@ export default function ROICalculator() {
     const [returnAmount, setReturnAmount] = useState('');
     const [roi, setRoi] = useState('');
     const [email, setEmail] = useState('');
+    const [physician, setPhysician] = useState('');
+    const [staff, setStaff] = useState('');
+    const [medicare, setMedicare] = useState('');
+    const [time, setTime] = useState('');
+    const [revenue, setRevenue] = useState('');
+    const [savings, setSavings] = useState('');
+
+    // Validate email
+    const checkEmail = (email) => {
+        return email
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+      };
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
+
+        if (checkEmail(email)) {
+            console.log("email is valid");
+        } else {
+            console.log("email is invalid");
+            setEmail('Invalid email address');
+        }
     };
   
+    // Calulate ROI
     const handleInvestmentChange = (e) => {
       setInvestment(e.target.value);
     };
@@ -25,17 +48,15 @@ export default function ROICalculator() {
       setRoi(roiValue.toFixed(2));
     };
 
-    const checkEmail = (email) => {
-        return email
-          .toLowerCase()
-          .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          );
-      };
+    /* Time Savings = (15 min)/(60 min) x (#of AWV for MA members)
+Revenue Generated =  (# of AWV for MA members) x ($75) 
+Savings = (15 min)/(60 min) x (Average of Physician and staff composition) */
   
     return (
       <div>
         <h2>ROI Calculator</h2>
+
+        {/* EMAIL */}
         <div>
             <label htmlFor="email">Email:</label>
             <input
@@ -44,6 +65,53 @@ export default function ROICalculator() {
                 value={email}
                 onChange={handleEmailChange}
             />
+            </div>
+
+        {/* PHYSICIAN COMPENSATION */}
+            <div>
+            <label htmlFor="physician">Physician Compensation:</label>
+            <input
+                type="email"
+                id="email"
+                // value={}
+                // onChange={}
+                placeholder="per hour"
+            />
+            </div>
+
+        {/* STAFF COMPENSATION */}
+            <div>
+            <label htmlFor="staff">Staff Compensation:</label>
+            <input
+                type="email"
+                id="email"
+                // value={}
+                // onChange={}
+                placeholder="per hour"
+            />
+            </div>
+
+        {/* ANNUAL WELLNESS VISITS FOR MEDICARE ADVANTAGE MEMBERS */}
+            <div>
+            <label htmlFor="medicare">Annual Wellness Visits for Medicare Advantage Members:</label>
+            <input
+                type="email"
+                id="email"
+                // value={}
+                // onChange={}
+                placeholder="visits/hour"
+            />
+            </div>
+
+        {/* CHECKBOX FOR WANTING TO SCHEDULE A DEMO */}
+            <div>
+            <label htmlFor="demo">Schedule a Demo:
+                <input type="checkbox" />
+            </label>
+            </div>
+
+
+            <div>
           <label htmlFor="investment">Investment:</label>
           <input
             type="number"
@@ -52,6 +120,7 @@ export default function ROICalculator() {
             onChange={handleInvestmentChange}
           />
         </div>
+
         <div>
           <label htmlFor="returnAmount">Return Amount:</label>
           <input
@@ -61,11 +130,16 @@ export default function ROICalculator() {
             onChange={handleReturnAmountChange}
           />
         </div>
+        
         <div>
           <button onClick={calculateROI}>Calculate ROI</button>
         </div>
         <div>
           <label>ROI:</label> {roi}%
+
+          {/* Time Savings = (15 min)/(60 min) x (#of AWV for MA members)
+Revenue Generated =  (# of AWV for MA members) x ($75) 
+Savings = (15 min)/(60 min) x (Average of Physician and staff composition) */}
         </div>
       </div>
     );
